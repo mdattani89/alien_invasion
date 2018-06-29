acbb0bc611f021afd20f0571de9303b4878045c1
Mihir Dattani <mihir.d.89@gmail.com>
2018-06-28 21:35:00 -0700

    Space ship now shoots bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 
 import game_functions as gf
 
@@ -16,13 +17,17 @@ def run_game():
 	#make a ship
 	ship = Ship(ai_settings,screen)
 
+	#Make a goup to store bullets in
+	bullets = Group()
+
 	
 
 	#Start the main loop for the game
 	while True:
-		gf.check_events(ship)
+		gf.check_events(ai_settings,screen,ship,bullets)
 		ship.update()
-		gf.update_screen(ai_settings,screen,ship)
+		bullets.update()
+		gf.update_screen(ai_settings,screen,ship,bullets)
 
 
 
--- settings.py
+++ settings.py
@@ -7,4 +7,14 @@ class Settings():
 		self.screen_width = 600
 		self.screen_heigth = 400
 		self.bg_color = (230,230,230)
+		
+		#ship setting
 		self.ship_speed_factor = 0.5
+
+		#bullet setting
+		self.bullet_speed_factor = .25
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = 60,60,60
+
+
