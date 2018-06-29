4a452b230f5a6dc6e9f765d532995afdc796ffa3
Mihir Dattani <mihir.d.89@gmail.com>
2018-06-28 20:51:44 -0700

    Space ship moves left and right

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+	"""A class to store all setting for Alien Invasion"""
+
+	def __init__(self):
+		"""Initialize the game's settings."""
+		#screen settings
+		self.screen_width = 600
+		self.screen_heigth = 400
+		self.bg_color = (230,230,230)
+		self.ship_speed_factor = 0.5
